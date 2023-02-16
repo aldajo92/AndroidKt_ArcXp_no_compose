03fcf401e01c38270345d7117364f0b086570861
Alejandro Gomez <alejodiscovery20@gmail.com>
2023-02-16 17:04:51 -0500

    removed bugs when no internet connection

--- app/src/main/java/com/example/arcxpcodechallenge/presentation/ui/main/MainViewModel.kt
+++ app/src/main/java/com/example/arcxpcodechallenge/presentation/ui/main/MainViewModel.kt
@@ -1,14 +1,15 @@
 package com.example.arcxpcodechallenge.presentation.ui.main
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
 import com.example.arcxpcodechallenge.framework.RequestStateResult
 import com.example.arcxpcodechallenge.presentation.models.UIState
 import com.example.arcxpcodechallenge.repositories.PostsRepository
 import com.example.arcxpcodechallenge.utils.toUIModel
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -16,18 +17,36 @@ class MainViewModel @Inject constructor(
     private val dataRepository: PostsRepository
 ) : ViewModel() {
 
-    private val dataFlow = dataRepository.getPostsFlow().map {
-        when (it) {
-            is RequestStateResult.Loading -> UIState.Loading
-            is RequestStateResult.Success -> {
-                val processedData = it.data?.map { post -> post.toUIModel() } ?: emptyList()
-                UIState.Success(processedData)
+//    private val dataFlow = dataRepository.getPostsFlow().map {
+//        when (it) {
+//            is RequestStateResult.Loading -> UIState.Loading
+//            is RequestStateResult.Success -> {
+//                val processedData = it.data?.map { post -> post.toUIModel() } ?: emptyList()
+//                UIState.Success(processedData)
+//            }
+//            else -> UIState.Error("Error")
+//        }
+//    }
+
+    private val _uiStateLiveData: MutableLiveData<UIState> = MutableLiveData()
+    val uiStatusLiveData: LiveData<UIState> = _uiStateLiveData
+//    val uiStatusLiveData: LiveData<UIState> = dataFlow.asLiveData()
+
+    fun getPosts() {
+        viewModelScope.launch {
+            dataRepository.getPostsFlow().collect {
+                _uiStateLiveData.value = when (it) {
+                    is RequestStateResult.Loading -> UIState.Loading
+                    is RequestStateResult.Success -> {
+                        val processedData = it.data?.map { post -> post.toUIModel() } ?: emptyList()
+                        UIState.Success(processedData)
+                    }
+                    else -> UIState.Error("Error")
+                }
             }
-            else -> UIState.Error("Error")
         }
     }
 
-    val uiStatusLiveData: LiveData<UIState> = dataFlow.asLiveData()
     fun getPostById(id: Int) = dataRepository.getPostById(id)
 
 }
